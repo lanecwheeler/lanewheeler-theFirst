c6b4f69a21ea232c80bf726158f36221c48458eb
lane.wheeler <lane.wheeler@egov.com>
2019-02-18 12:05:14 -0600

    taking out old error page component. just made it into a view, cause I`ma dumb

--- src/views/ErrorPage.vue
+++ src/views/ErrorPage.vue
@@ -2,7 +2,7 @@
     <v-container fluid fill-height class="home-hero" style="max-height: 100vh;" >
         <v-layout justify-center align-center column pa-5>
             <div class="display-3 font-weight-black white--text text-xs-center">404: Not Found</div>
-            <div class="display-1 font-weight-bold white--text text-xs-center">Woah there friend. I don't know what you're doin' there...</div>
+            <div class="display-1 font-weight-bold white--text text-xs-center">How did you even get here?</div>
         </v-layout>
     </v-container>
 </template>
